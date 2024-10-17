d0b9e341c73f8e17000483031bbfc2b63662f29c
Valur Viðarsson <valurvidarsson@askja-dw009.rhi.hi.is>
2024-10-17 16:07:12 +0000

    Klárað

--- scripts.js
+++ scripts.js
@@ -49,28 +49,100 @@ function split(str, separator = ' ') {
 // Grunnföll sem skilgreina á
 
 function longest(str) {
-  // Útfæra
-}
+  if (isString(str)) {
 
+  let longest = "";
+  const split1 = split(str);
+
+  for (let i = 0; i < split1.length; i++) {
+      if (split1[i].length > longest.length) {
+          longest = split1[i];
+      }
+  }
+
+  return longest;
+}
+return null:
+
+}
+  //Úfæra
 function shortest(str) {
-  // Útfæra
+
+  if (isString(str)) {
+
+  const split1 = split(str);
+  let shortest = split1[0];
+  
+
+  for (let i = 0; i < split1.length; i++) {
+      if (split1[i].length < shortest.length && split1[i] != " ") {
+          shortest = split1[i];
+      }
+    
+  }
+  return shortest;
+}
+return null;
 }
 
 function reverse(str) {
-  // Útfæra
-}
+if (isString(str)) {
+  const split = str.split('');
+  const reversed = split.reverse();
 
-function palindrome(str) {
+  return reversed.join('')
+}
+return null;
   // Útfæra
 }
+console.assert (reverse('halló') === 'óllah', 'reverse: snýr streng')
+
+// 1. (ekki palindrome) "halló" => false
+// 2. (palindrome) "hah" => true
+// 3. (óloglegt inntak) null / false / 0 => false
+// 4. "" ??? = => true
+// 5. "Hah" ??? => true
+function palindrome(str) {
+  if (isString(str) && str !== '') {
+    const reversed = reverse(str);
+    return (str.toLowerCase() === reversed.toLowerCase()) 
+  }
+return false;
+}
+console.assert (palindrome('halló') === false, 'halló á ekki palindrome')
+console.assert (palindrome('hah') === true, 'hah á að vera palindrome')
+console.assert (palindrome(null, false, 0) === false, 'ölöglegt inntök')
+console.assert (palindrome(' ') === true, 'tóm bil eru líka talinn með')
+console.assert (palindrome('Hah') === true, 'á að breyta streng í lágstafi')
 
 function vowels(str) {
-  // Útfæra
+  let vowelsCount = 0;
+    
+  for (let char of str) {
+      if (VOWELS.includes(char)) {
+          vowelsCount++;
+      }
+  }
+
+  return vowelsCount;
 }
+  // Útfæra
 
 function consonants(str) {
-  // Útfæra
+  let consonantCount = 0;
+    
+    for (let char of str) {
+        if (CONSONANTS.includes(char)) {
+            consonantCount++;
+        }
+    }
+
+    return consonantCount;
 }
+console.assert(consonants("aaab") === 1, "á að vera einn samhljóða")
+console.assert(consonants("") === 0, "ef það er tómt er 0")
+  // Útfæra
+
 
 //------------------------------------------------------------------------------
 // Leiðbeint ferli
